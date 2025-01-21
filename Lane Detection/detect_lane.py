433f828b7e38c6aff9d305bf25df826a489efddf
codeurient <codeurient@gmail.com>
2025-01-21 21:38:49 +0400

    findContours

--- Lane Detection/detect_lane.py
+++ Lane Detection/detect_lane.py
@@ -11,6 +11,11 @@ pts1 = np.float32([top_left, bottom_left, top_right, bottom_right])
 pts2 = np.float32([ [0,0], [0,480], [640,0], [640,480] ])  
 color = (0, 255, 0)
 
+# 2) Sonra, Sliding Window -umuzu While loop-u içində yaradacağıq. Çünki görüntüyə birdən çox kvadrat yerləşdirmək lazımdır. Ancaq ilk öncə pəncərələrin 
+# başlanğıc nöqtələrini təyin etməmiz lazımdır. 
+starting_y = 480 
+
+
 path = "test_videos\\road.mp4"
 cap = cv2.VideoCapture(path)
 ret, frame = cap.read()
@@ -44,7 +49,6 @@ while True:
 
     matrix = cv2.getPerspectiveTransform(pts1, pts2)
     transformed_frame = cv2.warpPerspective(frame, matrix, (640, 480)) 
-    
     transformed_frame_hsv = cv2.cvtColor(transformed_frame, cv2.COLOR_BGR2HSV)
 
     LOWER_H = cv2.getTrackbarPos("L-H", "Tracksbars") 
@@ -58,6 +62,47 @@ while True:
     UPPER = np.array([UPPER_H, UPPER_S, UPPER_V])
 
     transformed_frame_mask = cv2.inRange(transformed_frame_hsv, LOWER, UPPER)
+
+    histogram = np.sum(transformed_frame_mask[transformed_frame_mask.shape[0]//2:,:], axis=0) 
+    middle_point = np.int32(histogram[0]/2)
+
+    # 1) middle_point variable ilə görüntünü iki hissəyə ayırdıqdan sonra aşağıdakı kod ilə soldan başlayaraq sağ yarı ortaya qədər və 
+    # ortadan başlayaraq sağ sona qədər olan hissəni scan edirik. 
+    left_side = np.argmax(histogram[:middle_point])
+    right_side = np.argmax(histogram[middle_point:])
+
+    left_x = []
+    right_x = []
+
+    transformed_frame_mask_copy = transformed_frame_mask.copy()
+
+    while starting_y < 0:
+        # Left
+        img = transformed_frame_mask[starting_y-40:starting_y, left_side-50:left_side+50]
+        contours, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        for contour in contours:
+            M = cv2.moments(contour)
+            if M["m00"] != 0:
+                center_x = np.int32(M["m10"] / M["m00"])
+                center_y = np.int32(M["m01"] / M["m00"])
+                left_side = left_side - 50 + center_x
+        
+        # Right
+        img = transformed_frame_mask[starting_y-40:starting_y, right_side-50:right_side+50]
+        contours, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        for contour in contours:
+            M = cv2.moments(contour)
+            if M["m00"] != 0:
+                center_x = np.int32(M["m10"] / M["m00"])
+                center_y = np.int32(M["m01"] / M["m00"])
+                right_side = right_side - 50 + center_x
+
+
+
+
+
+    print("left_side: ", left_side)
+    print("right_side: ", right_side)
 
     cv2.imshow("Bird's eye view - BGR",  transformed_frame) 
     cv2.imshow("Bird's eye view - HSV",  transformed_frame_hsv) 
